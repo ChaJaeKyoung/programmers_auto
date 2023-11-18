d3c0a0f64d9633c0c5bb4d3290acd890705038da
ElliJenny <76678631+ChaJaeKyoung@users.noreply.github.com>
2023-11-18 18:43:28 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,3 +1,11 @@
 function solution(binomial) {
-    return eval(binomial)
+    const matchItUp = {
+        '+' : function(x,y) { return x + y },
+        '-' : function(x,y) { return x - y },
+        '*' : function(x,y) { return x * y },
+    }
+    const calcArr = binomial.split(' ')
+   
+    return matchItUp[calcArr[1]](+calcArr[0], +calcArr[2])
+    
 }
