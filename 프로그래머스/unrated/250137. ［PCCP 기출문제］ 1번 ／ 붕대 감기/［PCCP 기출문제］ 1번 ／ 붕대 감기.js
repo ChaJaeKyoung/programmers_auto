bc9caed8f265fad53dcabda310e4f336f70bb159
ElliJenny <76678631+ChaJaeKyoung@users.noreply.github.com>
2023-12-17 20:39:15 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,25 @@
+function solution(bandage, health, attacks) {
+   [시전시간, 초당회복량, 추가회복량] = bandage
+   let 현재체력 = health
+   let 연속성공 = 0
+   let 공격 = false
+   let count = 0
+   for(let 시간 = 0; 시간 <= attacks[attacks.length-1][0]; 시간++) {
+       if(현재체력 < 0) { break } 
+       if(시간 === attacks[count][0]) {
+           현재체력 -= attacks[count][1]
+           연속성공 = 0
+           count++
+       } else {
+           연속성공++
+           if(연속성공 === 시전시간) {
+               현재체력 += 추가회복량
+               연속성공 = 0
+           }
+           현재체력 += 초당회복량
+           if(현재체력 > health) 현재체력 = health
+       }
+       
+   }
+   return 현재체력 <= 0 ? -1 : 현재체력
+}
