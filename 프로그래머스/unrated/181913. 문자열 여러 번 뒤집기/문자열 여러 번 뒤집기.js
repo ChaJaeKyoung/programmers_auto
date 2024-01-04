19763c5a6110f01d9d3277524bb43105f4a9596a
ElliJenny <76678631+ChaJaeKyoung@users.noreply.github.com>
2024-01-04 23:39:36 +0900

    [level unrated] Title: 문자열 여러 번 뒤집기, Time: 29.85 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,14 +1,9 @@
 function solution(my_string, queries) {
     queries.forEach(el => {
-        let str = my_string.substring(el[0],el[1]+1)
-        let temp = ''
+        let str = [...my_string.substring(el[0],el[1]+1)].reverse()
         let tempArr = [...my_string]
-        for(let k = str.length - 1; k >= 0; k--) {
-            temp += str[k]
-        }
-        my_string = [...my_string]
         for(let i = el[0]; i <= el[1]; i++) {
-            tempArr[i] = temp[i-el[0]]
+            tempArr[i] = str[i-el[0]]
         }
         my_string = tempArr.join('')
     })
