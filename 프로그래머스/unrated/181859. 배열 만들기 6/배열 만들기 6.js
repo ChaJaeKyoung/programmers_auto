c75fd23910b024ca146de543786e89263d60f463
ElliJenny <76678631+ChaJaeKyoung@users.noreply.github.com>
2024-01-06 04:39:29 +0900

    [level unrated] Title: 배열 만들기 6, Time: 3.91 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    const stk = []
+    for(let i = 0; i < arr.length; ) {
+        if(stk.length === 0) {
+            stk.push(arr[i])
+            i++
+        } else if(stk.length > 0 && stk[stk.length - 1] === arr[i]) {
+            stk.pop()
+            i++
+        } else if(stk.length > 0 && stk[stk.length - 1] !== arr[i]) {
+            stk.push(arr[i])
+            i++
+        }
+    }
+    return stk.length > 0 ? stk : [-1]
+}
