3a4d06ecfba98cb055654113cf0e1823ee41b19a
Cha Jenny <76678631+ChaJaeKyoung@users.noreply.github.com>
2024-01-22 15:21:49 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,3 +1,9 @@
 function solution(n) {
-    return Number([...""+n].sort((a,b)=> b-a).reduce((acc,cur) => acc + cur))
+    const result = []
+    while(n > 0) {
+        result.push(n % 10)
+        n = Math.floor(n / 10)
+    }
+    return +result.sort((a,b)=>b-a).join('')
+    // return Number([...""+n].sort((a,b) => b - a).join(""))
 }
